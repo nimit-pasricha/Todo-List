9bc38f142e7ec07796a64a54f11cf9b79e22d8f4
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-21 08:31:31 +0400

    Create template index.html for html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,5 +7,5 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})],
 };
