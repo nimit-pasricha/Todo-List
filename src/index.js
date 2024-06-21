46f80aa2b63c0ff31497ba28efd69e3d27b19558
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-21 11:25:50 +0400

    Install css dev dependencies for webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import { addNewProject, deleteProject, getProjects, changeProjectName } from "./projects.js";
 import { createTodo, deleteTodo, editTodo, changeCompletedStatus } from "./todos.js";
+import "./style.css";
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})],
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
