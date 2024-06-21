cc3158f29d063ef6399a990adcfde73941396231
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-21 16:53:29 +0400

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { addNewProject, deleteProject, getProjects, changeProjectName } from "./projects.js";
-import { createTodo, deleteTodo, editTodo, changeCompletedStatus } from "./todos.js";
+import {
+  addNewProject,
+  deleteProject,
+  getProjects,
+  changeProjectName,
+} from "./projects.js";
+import {
+  createTodo,
+  deleteTodo,
+  editTodo,
+  changeCompletedStatus,
+} from "./todos.js";
 import "./style.css";
 import { addProfilePictureToSidebar } from "./ui-generator.js";
 
