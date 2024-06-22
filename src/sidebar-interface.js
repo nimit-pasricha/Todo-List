85c4b5a16410c88fcb886b1bf68b81c145c6a0c7
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-22 09:16:42 +0400

    Change new project prompt to be invisible until clicked

--- src/sidebar-interface.js
+++ src/sidebar-interface.js
@@ -13,29 +13,11 @@ function addProfilePictureToSidebar() {
 function addNewProjectWithAddButton() {
   const addProjectButton = document.querySelector(".add-project-button");
   addProjectButton.addEventListener("click", () => {
-    // remove the prompt if it is already present
-    const horizontalLineToDelete = document.querySelector("hr");
-    if (horizontalLineToDelete !== null) {
-      horizontalLineToDelete.remove();
-    }
-    const newProjectPromptToDelete = document.querySelector(
-      ".new-project-prompt"
+    const newProjectPrompt = document.querySelector(
+      ".invisible-new-project-prompt"
     );
-    if (newProjectPromptToDelete !== null) {
-      newProjectPromptToDelete.remove();
-    }
-
-    const projectList = document.querySelector(".project-list");
-    projectList.insertBefore(
-      document.createElement("hr"),
-      projectList.firstChild
-    );
-    const newProjectPrompt = document.createElement("li");
-    projectList.insertBefore(newProjectPrompt, projectList.firstChild);
+    newProjectPrompt.classList.remove("invisible-new-project-prompt");
     newProjectPrompt.className = "new-project-prompt";
-    newProjectPrompt.textContent = "Create new project...";
   });
-
-  // TODO: CLICKING BUTTON MULTIPLE TIMES SHOULD NOT CREATE MULTIPLE PROMPTS
 }
 export { addProfilePictureToSidebar, addNewProjectWithAddButton };
